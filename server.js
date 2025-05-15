42bad3749df656b234ed4cfadb9120ff9df544dd
memo <tuahil@gmail.com>
2025-05-14 18:11:22 -0600

    configure cors

--- server.js
+++ server.js
@@ -4,22 +4,9 @@ const cors = require('cors');
 const nodemailer = require('nodemailer');
 
 const app = express();
-const allowedOrigins = [
-    'http://localhost:3000',              // Express server
-    'http://127.0.0.1:5500',              // Live server or browser file preview
-    'https://guillermo-awesome-portfolio.netlify.app' // Deployed front-end
-];
-  
-app.use(cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    }
-}));
-  
+
+app.use(cors());
+    
 app.use(express.json());
 
 // Email endpoint
